bd235ad0781de875e0becd5d30eb84a4bffc1c48
exdserr <vaceslavsokin27@gmail.com>
2026-02-07 19:08:19 +0300

    add Talker Logger

--- lib/features/crypto_coin/bloc/crypto_coin_bloc.dart
+++ lib/features/crypto_coin/bloc/crypto_coin_bloc.dart
@@ -4,6 +4,8 @@ import 'package:crypto_coins_list/repositories/crypto_coins/abstract_coins_repos
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coins_details.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 part 'crypto_coin_event.dart';
 part 'crypto_coin_state.dart';
@@ -25,8 +27,13 @@ class CryptoCoinDetailsBloc
       }
       final coinsDetails = await coinsRepository.getCoinDetails();
       emit(CryptoCoinDetailsLoaded(coinsDetail: coinsDetails));
-    } catch (e) {
+    } catch (e,st) {
       emit(CryptoCoinDetailsLoadingFailure(exception: e));
+      GetIt.I<Talker>().handle(e, st);
     }
   }
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    super.onError(error, stackTrace);
+    GetIt.I<Talker>().handle(error, stackTrace);  }
 }
